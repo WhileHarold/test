9e5cb1be3793719b5a44f069b9c6820961dae5e9
WhileHarold <whileharold@gmail.com>
2024-06-13 09:42:53 +0900

    test 12

--- src/MonacoEditorComponent.js
+++ src/MonacoEditorComponent.js
@@ -1,29 +1,36 @@
-import React from "react";
-import MonacoEditor from "react-monaco-editor"; // 수정된 임포트
+import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
 
-const MonacoEditorComponent = () => {
-  const editorOptions = {
-    selectOnLineNumbers: true,
-    roundedSelection: false,
-    readOnly: false,
-    cursorStyle: "line",
-    automaticLayout: true,
+function MonacoEditorComponent() {
+  const [language, setLanguage] = useState("javascript"); // 기본 언어를 자바스크립트로 설정
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value); // 선택된 언어로 상태 업데이트
   };
 
-  function editorDidMount(editor, monaco) {
-    editor.focus();
-  }
-
   return (
-    <MonacoEditor
-      height="800px" // 적절한 높이 설정
-      language="javascript"
-      theme="vs-dark"
-      options={editorOptions}
-      onChange={(newValue, e) => console.log("onChange", newValue, e)}
-      editorDidMount={editorDidMount}
-    />
+    <div>
+      <select
+        onChange={handleLanguageChange}
+        value={language}
+        style={{ marginBottom: "10px" }}
+      >
+        <option value="javascript">JavaScript</option>
+        <option value="python">Python</option>
+        <option value="css">CSS</option>
+        <option value="html">HTML</option>
+        <option value="java">Java</option>
+        <option value="typescript">TypeScript</option>
+        <option value="json">JSON</option>
+      </select>
+      <Editor
+        height="90vh"
+        language={language}
+        defaultValue="// Type your code here"
+        theme="vs-dark"
+      />
+    </div>
   );
-};
+}
 
 export default MonacoEditorComponent;
